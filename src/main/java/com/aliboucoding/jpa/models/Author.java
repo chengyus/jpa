1c2377a2634019103fe02dcf6183d48adf54181f
Joseph Sung <chengyu.sung@gmail.com>
2024-05-06 01:23:06 +0800

    around 8:21:37

---
+++ src/main/java/com/aliboucoding/jpa/models/Author.java
@@ -0,0 +1,14 @@
+package com.aliboucoding.jpa.models;
+
+public class Author {
+
+  private Integer id;
+
+  private String firstName;
+
+  private String lastName;
+
+  private String email;
+
+  private int age;
+}

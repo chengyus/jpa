81d444aee72e59d610621d367d82b19fbd22cf36
Joseph Sung <chengyu.sung@gmail.com>
2024-05-06 09:32:02 +0800

    Author class with annotations.

--- src/main/java/com/aliboucoding/jpa/models/Author.java
+++ src/main/java/com/aliboucoding/jpa/models/Author.java
@@ -1,5 +1,12 @@
 package com.aliboucoding.jpa.models;
 
+import jakarta.persistence.Entity;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
 public class Author {
 
   private Integer id;

73a1c1179658d90e0342b45b8beeb1b14f8fb683
Joseph Sung <chengyu.sung@gmail.com>
2024-05-06 09:52:30 +0800

    primiary key Id annotation for Author.

--- src/main/java/com/aliboucoding/jpa/models/Author.java
+++ src/main/java/com/aliboucoding/jpa/models/Author.java
@@ -1,6 +1,7 @@
 package com.aliboucoding.jpa.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -9,6 +10,7 @@ import lombok.NoArgsConstructor;
 @Entity
 public class Author {
 
+  @Id
   private Integer id;
 
   private String firstName;

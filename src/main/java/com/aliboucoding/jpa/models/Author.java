c4e6dae14bea5c73a5c0ac869c15693d39081c7c
Joseph Sung <chengyu.sung@gmail.com>
2024-05-06 18:26:03 +0800

    tried TableGenerator annotation. around 8:53:26

--- src/main/java/com/aliboucoding/jpa/models/Author.java
+++ src/main/java/com/aliboucoding/jpa/models/Author.java
@@ -11,14 +11,22 @@ public class Author {
 
   @Id
   @GeneratedValue(
-    strategy = GenerationType.SEQUENCE,
-    generator = "author_sequence"
+    strategy = GenerationType.TABLE,
+    generator = "author_id_gen"
   )
-  @SequenceGenerator(
+  /*@SequenceGenerator(
     name = "author_sequence",
     sequenceName = "author_sequence",
     allocationSize = 1
+  )*/
+  @TableGenerator(
+    name = "author_id_gen",
+    table = "id_generator",
+    pkColumnName = "id_name",
+    valueColumnName = "id_value",
+    allocationSize = 1
   )
+
   private Integer id;
 
   private String firstName;

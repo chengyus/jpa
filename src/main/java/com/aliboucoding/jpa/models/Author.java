5b17dd1827e215ef2e7950abccc58df4301c6266
Joseph Sung <chengyu.sung@gmail.com>
2024-05-07 10:45:21 +0800

    restore to preferred sequence strategy; commented out tbl name line

--- src/main/java/com/aliboucoding/jpa/models/Author.java
+++ src/main/java/com/aliboucoding/jpa/models/Author.java
@@ -9,53 +9,38 @@ import java.time.LocalDateTime;
 @Data
 @NoArgsConstructor
 @Entity
+// @Table(name = "AUTHOR_TBL")
 public class Author {
 
   @Id
-  @GeneratedValue
-    /*(
-    strategy = GenerationType.TABLE,
-    generator = "author_id_gen"
-  )*/
-  /*@SequenceGenerator(
-    name = "author_sequence",
-    sequenceName = "author_sequence",
-    allocationSize = 1
-  )
-  @TableGenerator(
-    name = "author_id_gen",
-    table = "id_generator",
-    pkColumnName = "id_name",
-    valueColumnName = "id_value",
-    allocationSize = 1
-  )*/
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "author_seq")
+
+  @SequenceGenerator(name = "author_seq", sequenceName = "author_seq", allocationSize = 1)
+  /*
+   * @TableGenerator(
+   * name = "author_id_gen",
+   * table = "id_generator",
+   * pkColumnName = "id_name",
+   * valueColumnName = "id_value",
+   * allocationSize = 1
+   * )
+   */
 
   private Integer id;
 
-  @Column(
-    name = "f_name",
-    length = 35
-  )
+  @Column(name = "f_name", length = 35)
   private String firstName;
 
   private String lastName;
 
-  @Column(
-    unique = true,
-    nullable = false
-  )
+  @Column(unique = true, nullable = false)
   private String email;
 
   private int age;
 
-  @Column(
-    updatable = false,
-    nullable = false
-  )
+  @Column(updatable = false, nullable = false)
   private LocalDateTime createdAt;
 
-  @Column(
-    insertable = false
-  )
+  @Column(insertable = false)
   private LocalDateTime lastModified;
 }

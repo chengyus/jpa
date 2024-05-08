06630717cad3c260250930c437f1e7cfe18f9fc4
Joseph Sung <chengyu.sung@gmail.com>
2024-05-08 13:42:57 +0800

    changes to section and lecture; had to mvnw clean on the mbp

--- src/main/java/com/aliboucoding/jpa/models/Lecture.java
+++ src/main/java/com/aliboucoding/jpa/models/Lecture.java
@@ -3,6 +3,8 @@ package com.aliboucoding.jpa.models;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -20,4 +22,8 @@ public class Lecture {
   private Integer id;
 
   private String name;
+
+  @ManyToOne
+  @JoinColumn(name = "section_id")
+  private Section section;
 }
--- src/main/java/com/aliboucoding/jpa/models/Section.java
+++ src/main/java/com/aliboucoding/jpa/models/Section.java
@@ -1,10 +1,13 @@
 package com.aliboucoding.jpa.models;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -28,4 +31,7 @@ public class Section {
   @ManyToOne
   @JoinColumn(name = "course_id")
   private Course course;
+
+  @OneToMany
+  private List<Lecture> lectures;
 }

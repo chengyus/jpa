84989fe0d0b355b6a886e7456afef2f40a7fc8d4
Joseph Sung <chengyu.sung@gmail.com>
2024-05-09 11:46:39 +0800

    fixing joincoulmn in Resource

--- src/main/java/com/aliboucoding/jpa/models/Resource.java
+++ src/main/java/com/aliboucoding/jpa/models/Resource.java
@@ -3,6 +3,8 @@ package com.aliboucoding.jpa.models;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -21,4 +23,8 @@ public class Resource {
   private String name;
   private int size;
   private String url;
+
+  @OneToOne
+  @JoinColumn(name = "lecture_id")
+  private Lecture lecture;
 }

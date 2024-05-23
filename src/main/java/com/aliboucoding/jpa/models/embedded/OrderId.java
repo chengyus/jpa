22de951c7a6d121bf3a1b0bddb5bc8360204c286
Joseph Sung <chengyu.sung@gmail.com>
2024-05-23 18:18:17 +0800

    missed that Serializable when watching Video on iPadPro

--- src/main/java/com/aliboucoding/jpa/models/embedded/OrderId.java
+++ src/main/java/com/aliboucoding/jpa/models/embedded/OrderId.java
@@ -5,13 +5,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Embeddable
-public class OrderId {
+public class OrderId implements Serializable {
 
   private String username;
   private LocalDateTime orderDate;

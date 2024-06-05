047f828964dd6a9083afb8dcec4366078b67bbb9
Joseph Sung <chengyu.sung@gmail.com>
2024-06-05 09:59:52 +0800

    updateAllAuthorsAges ; 12:15:20

--- src/main/java/com/aliboucoding/jpa/JpaApplication.java
+++ src/main/java/com/aliboucoding/jpa/JpaApplication.java
@@ -43,7 +43,13 @@ public class JpaApplication {
         .age(34)
         .email("contact@aliboucoding.com")
         .build();
-      repository.save(author);
+      //repository.save(author);
+
+      // update Author a. set a.age = 22 where a.id = 1
+      //repository.updateAuthor(22, 1);
+
+      // update all authors
+      repository.updateAllAuthorsAges(99);
     };
   }
 }
--- src/main/java/com/aliboucoding/jpa/repositories/AuthorRepository.java
+++ src/main/java/com/aliboucoding/jpa/repositories/AuthorRepository.java
@@ -4,9 +4,23 @@ import com.aliboucoding.jpa.models.Author;
 
 import java.util.List;
 
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface AuthorRepository extends JpaRepository<Author, Integer> {
+
+  @Modifying
+  @Transactional
+  @Query("update Author a set a.age = :age where a.id = :id")
+  int updateAuthor(int age, int id);
+
+  @Modifying
+  @Transactional
+  @Query("update Author a set a.age = :age")
+  void updateAllAuthorsAges(int age);
+
   // select * from author where first_name = 'ali'
   List<Author> findAllByFirstName(String fn);
 
